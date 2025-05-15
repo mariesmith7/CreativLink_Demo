1cc015bfb60b4e660b1636f9fe074d4d53a3805b
mariesmith7 <mariesmith1252@gmail.com>
2025-05-15 09:48:16 -0400

    updated css

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -25,8 +25,75 @@ document.addEventListener('DOMContentLoaded', () => {
         return;
       }
 // type events in here* 
-      resultsContainer.innerHTML = `<p>Event 1 happy days</p>
-      <span> come visit today</span>`;
+      resultsContainer.innerHTML = `
+
+
+     <section class="eventdescription">
+            <img src="/imgs/mam.png">
+            <span class="eventtitle"> <strong> MAM Art Gala </strong> </span>
+            <p class="eventdesc"> The Montclair Art Museum invites you to an evening of artistic celebration at our
+              annual
+              Spring Gala. Dine in MAM's beautifully transformed Leir Hall as we honor three luminaries whose vision and
+              dedication have strengthened our cultural landscape. </p>
+            <span class="date"> May 17, 2025 6pm -10pm </span>
+            <span class="eventlocation"> <strong> Montclair Art Museum </strong></span>
+            <span class="eventaddress"> 3 S Mountain Ave, Montclair, NJ 07042 </span>
+            <span class="eventaddinfo"> General Admission: FREE </span>
+            <span class="moreinfo"> <a
+                href="https://www.montclairartmuseum.org/events/events-and-programs/mam-art-gala">Learn More</a> </span>
+          </section>
+
+
+          <section class="eventdescription">
+            <img src="imgs/bridgeart.png">
+            <span class="eventtitle"> <strong> Bridge Art Exchange June 2025 </strong></span>
+            <p class="eventdesc"> This event provides a unique opportunity for artists to showcase their work, share
+              insights into their creative processes + engage w/artist </p>
+            <span class="date"> Saturday, June 14 · 10am - 12pm EDT </span>
+            <span class="eventlocation"> <strong> Bridge Art Gallery </strong></span>
+            <span class="eventaddress"> 213 North Market Street Wilmington, DE 19801 </span>
+            <span class="eventaddinfo"> General Admission: FREE </span>
+            <span class="moreinfo"> <a
+                href="https://www.eventbrite.com/e/bridge-art-exchange-june-2025-tickets-1361714878859?aff=ebdssbdestsearch">Learn
+                More</a> </span>
+          </section>
+
+
+          <section class="eventdescription">
+            <img src="/imgs/art1940.png">
+            <span class="eventtitle"> <strong> Boom: Art and Design in the 1940s </strong></span>
+            <p class="eventdesc"> This exhibition will showcase art from across the decade, featuring works drawn
+              entirely
+              from the museum’s permanent collections. </p>
+            <span class="date"> Ongoing - September 1, 2025 </span>
+            <span class="eventlocation"> <strong> Philadelphia Museum of Art</strong> </span>
+            <span class="eventaddress"> 2600 Benjamin Franklin Pkwy, Philadelphia, PA 19130 </span>
+            <span class="eventaddinfo"> General Admission: $35 </span>
+            <span class="moreinfo"> <a href="https://philamuseum.org/calendar/exhibition/boom-art-design-1940s">Learn
+                More</a> </span>
+          </section>
+
+
+
+          <section class="eventdescription">
+            <img src="/imgs/delart.png">
+            <span class="eventtitle"> Imprinted: Illustrating Race </span>
+            <p class="eventdesc"> Imprinted: Illustrating Race with co-curator
+              Robyn
+              Phillips-Pendleton. The exhibition honors Rockwell’s powerful
+              images supporting the Civil Rights Movement, displaying his work within a sweeping historical survey of
+              American illustration ... </p>
+            <span class="date"> Oct 18, 2025 - Mar 1, 2026 </span>
+            <span class="eventlocation"> Delaware Art Museum </span>
+            <span class="eventaddress"> 2301 Kentmere Pkwy, Wilmington, DE 19806 </span>
+            <span class="eventaddinfo"> General Admission: $18 </span>
+            <span class="moreinfo"> <a href="https://delart.org/event/imprinted/">Learn More</a> </span>
+          </section>
+
+
+          <span> View more here </span>
+      
+      `;
 
       // try {
       //   // fetch api to create a http post req 
